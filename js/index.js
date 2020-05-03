de8d8edefd8bf5f2e5652b78b8f8c3c1c210d9b0
sourav kumar <sourav.baranwal@gmail.com>
2020-05-03 18:02:24 +0530

    Updated method comments.

--- js/index.js
+++ js/index.js
@@ -3,73 +3,64 @@ var totalPages;
 var data;
 const slideIndex = 5;
 
+/**
+ * Description : This method is called after upload csv button click and
+ * it parses the csv file to a JS file object
+ */
 function submitAction() {
   let filesObj = document.getElementById("fileToUpload");
   let csvFile = filesObj.files[0];
-  console.log(csvFile);
+
   let reader = new FileReader();
   if (csvFile) {
     reader.readAsText(csvFile);
   } else {
-    console.error("please select csv file");
+    console.error("please select csv file to upload");
+    alert("please select csv file to upload");
   }
 
   reader.onload = () => {
     let fileData = reader.result;
     data = parseCSV(fileData);
-    console.log("parse data", data);
+
     displaySlider(data);
   };
 
   reader.onerror = () => {
-    console.log(reader.error);
+    console.error(reader.error);
   };
 }
 
-let handleClick = (data, pageNumber) => {
-  console.log(data);
-  currPage = pageNumber;
-  console.log("currPage", currPage);
-  if (currPage != -1) {
-    console.log("slice first arg :", currPage * slideIndex);
-    console.log("slice 2nd arg :", (currPage + 1) * slideIndex);
-    const paginatedData = data.slice(
-      currPage * slideIndex,
-      (currPage + 1) * slideIndex
-    );
-    console.log("paginateData with next ", paginatedData);
-    setNavigationAttr(currPage);
-    displaySliderContent(paginatedData);
-  }
-};
+/**
+ * Description: This method is use to parse the csv file object to slider data
+ * @param {*} dataStr
+ * @param {*} delimiter
+ */
+function parseCSV(dataStr, delimiter = ",") {
+  const headerTitleRow = dataStr
+    .slice(0, dataStr.indexOf("\n"))
+    .split(delimiter);
 
-function setNavigationAttr(currPage) {
-  let prevBtnClass = currPage == 0 ? "prev-btn disabled" : "prev-btn ";
-  let nextBtnClass = "";
-  let totalPages = Math.ceil(data.length / slideIndex);
-  if (totalPages == 1) {
-    nextBtnClass = "next-btn disabled";
-  } else {
-    if (currPage == 0) {
-      nextBtnClass = "next-btn";
-    } else if (currPage == totalPages - 1) {
-      nextBtnClass = "next-btn disabled";
-    }
-  }
+  const rows = dataStr.slice(dataStr.indexOf("\n") + 1).split("\n");
 
-  let prevBtn = document.getElementById("prevBtn");
-  let nextBtn = document.getElementById("nextBtn");
+  return rows.map((row) => {
+    const values = row.split(delimiter);
 
-  prevBtn.setAttribute("class", prevBtnClass);
-  nextBtn.setAttribute("class", nextBtnClass);
+    return headerTitleRow.reduce((object, curr, i) => {
+      object[curr.trim()] = values[i].trim();
+      return object;
+    }, {});
+  });
 }
 
+/**
+ * Description : This method is used to populate the slider
+ * csv file is parsed
+ * @param {*} data
+ */
 function displaySlider(data) {
-  console.log(data, "DATA FROM CSV");
   currPage = 0;
-  // let disablePrev = currPage == 0 ? "prev-btn disabled" : "prev-btn ";
-  // let disableNext =
-  //   currPage == data.length % slideIndex ? "next-btn disabled" : "next-btn";
+
   let sliderHtml = `<div id="slider" class="slider">
   <div class='button-container'>
   <button id="prevBtn" onClick='handleClick(data,currPage-1)' >
@@ -98,13 +89,43 @@ function displaySlider(data) {
   displaySliderContent(paginatedData);
 }
 
+/**
+ * Description : This method is used to set the active/disabled status
+ * of the control buttons (Next & Prev)
+ * @param {*} currPage
+ */
+function setNavigationAttr(currPage) {
+  let prevBtnClass = currPage == 0 ? "prev-btn disabled" : "prev-btn ";
+  let nextBtnClass = "";
+  let totalPages = Math.ceil(data.length / slideIndex);
+  if (totalPages == 1) {
+    nextBtnClass = "next-btn disabled";
+  } else {
+    if (currPage == 0) {
+      nextBtnClass = "next-btn";
+    } else if (currPage == totalPages - 1) {
+      nextBtnClass = "next-btn disabled";
+    }
+  }
+
+  let prevBtn = document.getElementById("prevBtn");
+  let nextBtn = document.getElementById("nextBtn");
+
+  prevBtn.setAttribute("class", prevBtnClass);
+  nextBtn.setAttribute("class", nextBtnClass);
+}
+
+/**
+ * Description : This method is used to attached the paginated slide
+ * content into the slider
+ * @param {*} paginatedData
+ */
 function displaySliderContent(paginatedData) {
   const sliderContainer = document.getElementById("slider-container");
   const slideItemContainer = document.getElementById("slider-item-container");
   slideItemContainer.innerHTML = "";
-  console.log("paginateData: ", paginatedData);
+
   paginatedData.forEach((slide) => {
-    console.log(slide);
     let slideItem = document.createElement("div");
     slideItem.setAttribute("class", "slider-item");
     const slideContent = `
@@ -121,23 +142,24 @@ function displaySliderContent(paginatedData) {
     slideItem.innerHTML = slideContent;
     slideItemContainer.appendChild(slideItem);
   });
-  console.log(sliderContainer);
 }
 
-function parseCSV(dataStr, delimiter = ",") {
-  const headerTitleRow = dataStr
-    .slice(0, dataStr.indexOf("\n"))
-    .split(delimiter);
-  //    console.log(headerTitleRow);
-  const rows = dataStr.slice(dataStr.indexOf("\n") + 1).split("\n");
+/**
+ * Description : This method is used to handle the Prev/Next button
+ * click event
+ * @param {*} data
+ * @param {*} pageNumber
+ */
+let handleClick = (data, pageNumber) => {
+  currPage = pageNumber;
 
-  return rows.map((row) => {
-    // Convert to 2D array
-    const values = row.split(delimiter);
-    // Convert array to object
-    return headerTitleRow.reduce((object, curr, i) => {
-      object[curr.trim()] = values[i].trim();
-      return object;
-    }, {});
-  });
-}
+  if (currPage != -1) {
+    const paginatedData = data.slice(
+      currPage * slideIndex,
+      (currPage + 1) * slideIndex
+    );
+
+    setNavigationAttr(currPage);
+    displaySliderContent(paginatedData);
+  }
+};
